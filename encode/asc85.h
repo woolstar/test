79e9577ced3eac9d5874d2612a219c28edede5fb
woolstar <jdwoolverton@gmail.com>
2013-07-15 22:45:04 -0700

    ascii85 rfc1924 pack and block encode routines

---
+++ encode/asc85.h
@@ -0,0 +1,14 @@
+
+		//// pack operators
+
+extern const char *    pack_a85( unsigned long lval) ;
+extern const char *    pack_a85e( unsigned long lval) ;  // pack without '.' shortcut
+
+extern unsigned long   unpack_a85( const char * astr) ;
+extern unsigned long   unpack_a85x( const char * astr, int * zlen ) ;	// unpack with fragment tracking
+
+		//// array operators
+
+void	encode_asc85(char * zbuf, int asz, const unsigned char * asrc, int alen)
+int		decode_asc85(unsigned char * zdest, int asz, const char * asrc)
+
