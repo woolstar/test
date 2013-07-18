7aba9532576cf0687b91f411d972c337b3e8ee30
woolstar <jdwoolverton@gmail.com>
2013-07-18 11:13:26 -0700

    better name buffer operators

--- encode/asc85.h
+++ encode/asc85.h
@@ -7,7 +7,7 @@ extern const char *    pack_a85e( unsigned long lval) ;  // pack without '.' sho
 extern unsigned long   unpack_a85( const char * astr) ;
 extern unsigned long   unpack_a85x( const char * astr, int * zlen ) ;	// unpack with fragment tracking
 
-		//// array operators
+		//// buffer operators
 
 extern void	encode_asc85(char * zdest, int asz, const unsigned char * asrc, int alen) ;
 extern int	decode_asc85(unsigned char * zdest, int asz, const char * asrc) ;
