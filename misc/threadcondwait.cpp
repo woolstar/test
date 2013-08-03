b5d9071b1eb825a3802ce17aa81cebf2210c1285
woolstar <jdwoolverton@gmail.com>
2013-08-02 18:38:26 -0700

    factor out non-portable waits

--- misc/threadcondwait.cpp
+++ misc/threadcondwait.cpp
@@ -4,13 +4,39 @@
 #include <thread>
 #include <chrono>
 
+#ifdef	WIN32
+#	include <windows.h>
+#endif
+
 	// stolen straight from http://en.cppreference.com/w/cpp/thread/condition_variable/wait_for
 	// just to make sure the parts were there
 	// compiled: g++ -std=gnu++0x -pthread threadcondwait.cpp
- 
+
+	// hacked up to get around platform wait_for issues.
+
+	static void	wait(int ams)
+	{
+#ifdef	CYGWIN
+	#include <time.h>
+
+		struct timespec delay= { 0, 0} ;
+
+		delay.tv_nsec= 1000000 * ams ;
+		nanosleep( &delay, NULL) ;
+
+#else
+#ifdef	WIN32
+		Sleep( ams) ;
+#else
+		std::this_thread::sleep_for(std::chrono::milliseconds(ams));
+#endif
+#endif
+	}
+
+
 std::condition_variable cv;
 std::mutex cv_m;
-std::atomic<int> i = ATOMIC_VAR_INIT(0);
+std::atomic<int> i = 0 ;
  
 void waits(int idx)
 {
@@ -20,18 +46,17 @@ void waits(int idx)
     else
         std::cerr << "Thread " << idx << " timed out. i == " << i << '\n';
 }
- 
 void signals()
 {
-    std::this_thread::sleep_for(std::chrono::milliseconds(120));
+    wait( 120) ;
     std::cerr << "Notifying...\n";
     cv.notify_all();
-    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    wait( 120) ;
     i = 1;
     std::cerr << "Notifying again...\n";
     cv.notify_all();
 }
- 
+
 int main()
 {
     std::thread t1(waits, 1), t2(waits, 2), t3(waits, 3), t4(signals);
