43871a5470ff5312a63255cf916f41293935778b
woolstar <jdwoolverton@gmail.com>
2013-08-10 22:24:12 -0700

    add formatting

--- misc/addl.js
+++ misc/addl.js
@@ -8,6 +8,6 @@ var isum= 0 ;
 rl.on('line', function(str) {
 	str.trim() ;
 	if ( str.match(/(\[\d\.\-\+]+)/) ) { isum += + str ; }
-		else { console.log("\t" + isum) ; }
-	}).on('close', function() { console.log("  ====\t" + isum) ;  process.exit( 0) ; } ) ;
+		else { console.log("\t" + isum.toPrecision(2)) ; }
+	}).on('close', function() { console.log("  ====\t" + isum.toPrecision(2)) ;  process.exit( 0) ; } ) ;
 
