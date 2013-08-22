42905117e4e11143844a34387bff0882a01dfd1d
woolstar <jdwoolverton@gmail.com>
2013-08-22 16:35:22 -0700

    fix regex and switch to toFixed formatting

--- misc/addl.js
+++ misc/addl.js
@@ -7,7 +7,7 @@ var isum= 0 ;
 
 rl.on('line', function(str) {
 	str.trim() ;
-	if ( str.match(/(\[\d\.\-\+]+)/) ) { isum += + str ; }
-		else { console.log("\t" + isum.toPrecision(2)) ; }
-	}).on('close', function() { console.log("  ====\t" + isum.toPrecision(2)) ;  process.exit( 0) ; } ) ;
+	if ( str.match(/([\d\.\-\+]+)/) ) { isum += + str ; }
+		else { console.log("\t" + isum.toFixed(2)) ; }
+	}).on('close', function() { console.log("  ====\t" + isum.toFixed(2)) ;  process.exit( 0) ; } ) ;
 
