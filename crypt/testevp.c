6152877abe4bde5302960ff871d25996422218e3
woolstar <jdwoolverton@gmail.com>
2013-09-02 12:55:51 -0700

    load cipher list

--- crypt/testevp.c
+++ crypt/testevp.c
@@ -13,6 +13,9 @@ int main(int N, char ** S)
 	unsigned char iv[]= "12345678" ;
 
 	EVP_CIPHER_CTX ctx;
+
+	OpenSSL_add_all_ciphers() ;
+
 	EVP_CipherInit_ex(&ctx, EVP_bf_cbc(), NULL, NULL, NULL, 1);
 	EVP_CIPHER_CTX_set_key_length(&ctx, 10);
 	EVP_CipherInit_ex(&ctx, NULL, NULL, key, iv, -1);
@@ -20,6 +23,7 @@ int main(int N, char ** S)
 	// ... EVP_CipherFinal_ex(&ctx, outbuf, &outlen)
 
 	EVP_CIPHER_CTX_cleanup(&ctx);
+	EVP_cleanup() ;
 
 }
 
