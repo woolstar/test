8e1e8f504bb23bf2aa44910629047ddd00d2a5f8
woolstar <jdwoolverton@gmail.com>
2013-10-21 09:39:13 -0700

    use new for range form for test setup

--- misc/testitr.cpp
+++ misc/testitr.cpp
@@ -14,17 +14,11 @@ class	testv : public edata_treeitr<testv>::node
 
 int	main(int n, char **)
 {
-	int ilist[]= { 10, 11, 8, 4, 20, 40, 31, 32, 33, 30, 2, 1, 0 } ;
-	int * istep ;
-
 	class	testtree : public edata_aatree<testv>, public edata_treeitr<testv>
-	{
-	}
+	{ }
 		_tree ;
 
-	for ( istep= ilist; ( * istep ) ; istep ++ ) {
-		_tree.add( new testv(* istep)) ;
-	}
+	for ( auto i : { 10, 11, 8, 4, 20, 40, 31, 32, 33, 30, 2, 1 } ) { _tre.add( new testv( i )) ; }
 
 	{
 		edata_treeitr<testv>::iptr	istep( _tree) ;
