075d67b7dc8398ab753a55eda00595bfb5aac116
woolstar <jdwoolverton@gmail.com>
2013-11-01 11:27:21 -0700

    switch to testsort3 and adjust ranges

--- misc/sortharness.pl
+++ misc/sortharness.pl
@@ -6,7 +6,7 @@ my $k= log( 10) ;
 	sub build_gcc
 	{
 		my ($lvl, $slow, $f)= @_ ;
-		system("g++-4.8 -std=c++11 -O$lvl -DSLOW_TESTS=$slow testsort2.cpp -lm -o testsort" ) ;
+		system("g++-4.8 -std=c++11 -O$lvl -DSLOW_TESTS=$slow testsort3.cpp -lm -o testsort" ) ;
 		$f ||= 'f-' unless $slow ;
 		return "${f}gcc$lvl"
 	}
@@ -14,7 +14,7 @@ my $k= log( 10) ;
 	sub build_clang
 	{
 		my ($lvl, $slow, $f)= @_ ;
-		system("clang++ -std=c++11 -O$lvl -DSLOW_TESTS=$slow testsort2.cpp -lstdc++ -lm -o testsort" ) ;
+		system("clang++ -std=c++11 -O$lvl -DSLOW_TESTS=$slow testsort3.cpp -lstdc++ -lm -o testsort" ) ;
 		$f ||= 'f-' unless $slow ;
 		return "${f}clang$lvl" ;
 	}
@@ -27,7 +27,7 @@ sub	run_test
 	&build_clean ;
 	my $grp= $buildr->() ;
 
-	$min ||= 80 if $max >= 100 ;
+	$min ||= 80 if $max > 90 ;
 	$min ||= 60 ;
 
 	print "GRP $grp\n" ;
@@ -43,17 +43,17 @@ sub	run_test
 
 {
 		## test the fast sorts up to 100MM
-	run_test( 160, sub { build_gcc( 3, 0) } ) ;
-	run_test( 160, sub { build_clang( 3, 0) } ) ;
+	run_test( 180, sub { build_gcc( 3, 0) } ) ;
+	run_test( 180, sub { build_clang( 3, 0) } ) ;
 
 		## test all sorts at different optimization levels and compilers
 
 	run_test( 80, sub { build_gcc( 0, 1) } ) ;
-	run_test( 90, sub { build_gcc( 2, 1) } ) ;
-	run_test( 100, sub { build_gcc( 3, 1) } ) ;
+	run_test( 95, sub { build_gcc( 2, 1) } ) ;
+	run_test( 95, sub { build_gcc( 3, 1) } ) ;
 
 	run_test( 80, sub { build_clang( 0, 1) } ) ;
-	run_test( 90, sub { build_clang( 2, 1) } ) ;
-	run_test( 100, sub { build_clang( 3, 1) } ) ;
+	run_test( 95, sub { build_clang( 2, 1) } ) ;
+	run_test( 95, sub { build_clang( 3, 1) } ) ;
 }
 
