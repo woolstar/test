2c2e2dd158dbae4d23617decbdd0bb311d155d36
woolstar <jdwoolverton@gmail.com>
2020-12-06 21:33:41 -0700

    Upgrade addl
    Add space chomp
    count number of items
    count negative values as numbers
    From alternate version dated Mar 2008

--- misc/addl.c
+++ misc/addl.c
@@ -2,24 +2,26 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-int	main()
+main()
 {
-	char s[64], * p ;
+	char s[128], * p, c ;
 	double dval, dsum ;
+	int i= 0 ;
 
 	dsum= 0 ;
 	while ( fgets(s, sizeof(s), stdin)) {
-		if ( isdigit( *s )) {
-			dval= atof( s) ;
+		p = s ;
+		while ((c= *p) && ((' ' == c) || ('\t' == c))) p ++ ;
+
+		if ( isdigit( *p ) || ('-' == * p)) {
+			dval= atof( p) ;
 			dsum += dval ;
+			i ++ ;
 		}
-		else {
-			printf("\t%.2f\n", dsum) ;
-		}
+			else { printf("\t = %.2f\n", dsum) ; }
 	}
 
-	printf("  ====\t%.2f\n", dsum) ;
-
-	return 0;
+	if ( i )
+		{ printf("  ==== [%d]\t%.2f\n", i, dsum) ; }
 }
 
