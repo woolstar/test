acf130350dd64c4ce2c11bc1af8a3656b0a02a0c
jdw <woolstar@iamj>
2021-02-04 20:00:34 -0500

    Update with JSServ hack

--- julia/test_webgl.jl
+++ julia/test_webgl.jl
@@ -1,8 +1,11 @@
 ### A Pluto.jl notebook ###
-# v0.12.18
+# v0.12.20
 
 using Markdown
 using InteractiveUtils
+
+# ╔═╡ a35dfdd8-674c-11eb-0dd8-bb9478231fdd
+using JSServe ; Page(exportable=true, offline=true)
 
 # ╔═╡ b0c1d882-4e24-11eb-0162-eff5edb3e259
 begin
@@ -20,9 +23,11 @@ begin
 	
 	scene= Scene()
 	meshscatter!( scene, pts, markersize= 0.02, color=:red )
+	scene
 end
 
 # ╔═╡ Cell order:
+# ╠═a35dfdd8-674c-11eb-0dd8-bb9478231fdd
 # ╠═b0c1d882-4e24-11eb-0162-eff5edb3e259
 # ╟─26f89c58-4e4c-11eb-0fdc-e92a81ba81ab
 # ╠═5a4dd6c2-4e4c-11eb-28f4-35b8ff167b3f
